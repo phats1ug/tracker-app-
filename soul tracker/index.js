e491025289bf42d8798f88d40f88cadaefdc9552
I Macdonald <95100364+phats1ug@users.noreply.github.com>
2022-06-13 00:02:09 +0200

    Add files via upload

---
+++ soul tracker/index.js
@@ -0,0 +1,17 @@
+// make it that you can log in the souls that go through the gates.
+
+let count = 0;
+let countEl = document.getElementById("count-el")
+
+function increment() {
+	count += 1
+	
+	countEl.innerText = count
+
+}
+
+function save() {
+	console.log(count)
+}
+
+save()
